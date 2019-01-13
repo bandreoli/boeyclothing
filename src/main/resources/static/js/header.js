959a4167af4e5ce84356a51346ab74b8abd1185a
Blaine Andreoli <blaine.andreoli@yahoo.com>
2019-01-12 20:26:01 -0500

    Created javascript folder

---
+++ src/main/resources/static/js/header.js
@@ -0,0 +1,15 @@
+function openMenMenu() {
+	document.getElementById("menMenu").style.display = "block";
+}
+
+function closeMenMenu() {
+	document.getElementById("menMenu").style.display = "none";
+}
+
+function openWomenMenu() {
+	document.getElementById("womenMenu").style.display = "block";
+}
+
+function closeWomenMenu() {
+	document.getElementById("womenMenu").style.display = "none";
+}
