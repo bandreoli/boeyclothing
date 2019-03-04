d71d6453e57be3a4bbb6d8e6682bdfcac353b047
Blaine Andreoli <blaine.andreoli@yahoo.com>
2019-03-04 16:37:08 -0500

    removed gray header items

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -3,48 +3,28 @@ function menMenuOpen() {
 	document.getElementById("menMenu").style.display = "block";
 	document.getElementById("arrivalsMenu").style.display = "none";
 	document.getElementById("saleMenu").style.display = "none";
-	document.getElementById("womenButton").style.backgroundColor = "white";
-	document.getElementById("menButton").style.backgroundColor = "lightgray";
-	document.getElementById("arrivalsButton").style.backgroundColor = "white";
-	document.getElementById("saleButton").style.backgroundColor = "white";
 }
 function womenMenuOpen() {
 	document.getElementById("menMenu").style.display = "none";
 	document.getElementById("arrivalsMenu").style.display = "none";
 	document.getElementById("womenMenu").style.display = "block";
 	document.getElementById("saleMenu").style.display = "none";
-	document.getElementById("menButton").style.backgroundColor = "white";
-	document.getElementById("womenButton").style.backgroundColor = "lightgray";
-	document.getElementById("arrivalsButton").style.backgroundColor = "white";
-	document.getElementById("saleButton").style.backgroundColor = "white";
 }
 function arrivalsMenuOpen() {
 	document.getElementById("menMenu").style.display = "none";
 	document.getElementById("womenMenu").style.display = "none";
 	document.getElementById("arrivalsMenu").style.display = "block";
 	document.getElementById("saleMenu").style.display = "none";
-	document.getElementById("menButton").style.backgroundColor = "white";
-	document.getElementById("womenButton").style.backgroundColor = "white";
-	document.getElementById("arrivalsButton").style.backgroundColor = "lightgray";
-	document.getElementById("saleButton").style.backgroundColor = "white";
 }
 function saleMenuOpen() {
 	document.getElementById("menMenu").style.display = "none";
 	document.getElementById("womenMenu").style.display = "none";
 	document.getElementById("arrivalsMenu").style.display = "none";
 	document.getElementById("saleMenu").style.display = "block";
-	document.getElementById("menButton").style.backgroundColor = "white";
-	document.getElementById("womenButton").style.backgroundColor = "white";
-	document.getElementById("arrivalsButton").style.backgroundColor = "white";
-	document.getElementById("saleButton").style.backgroundColor = "lightgray";
 }
 function closeMenus() {
 	document.getElementById("menMenu").style.display = "none";
 	document.getElementById("womenMenu").style.display = "none";
 	document.getElementById("arrivalsMenu").style.display = "none";
 	document.getElementById("saleMenu").style.display = "none";
-	document.getElementById("menButton").style.backgroundColor = "white";
-	document.getElementById("womenButton").style.backgroundColor = "white";
-	document.getElementById("arrivalsButton").style.backgroundColor = "white";
-	document.getElementById("saleButton").style.backgroundColor = "white";
 }
